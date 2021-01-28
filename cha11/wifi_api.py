1aa21d83ff881f986e5d2531c2b4cb79163677bd
cantoluna <cantoluna1@gmail.com>
2021-01-29 08:39:26 +0900

    Upload files

---
+++ cha11/wifi_api.py
@@ -0,0 +1,11 @@
+import urllib.request
+import json
+
+url = 'http://openapi.seoul.go.kr:8088/(인증키)/json/PublicWiFiPlaceInfo/1/500'
+response = urllib.request.urlopen(url)
+json_str = response.read().decode('utf-8')
+data = json.loads(json_str)
+print(json.dumps(data, indent=4, ensure_ascii=False))
+
+#for row in data['PublicWiFiPlaceInfo']['row']:
+#  print(row['GU_NM'])
